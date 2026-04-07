b195b2320238b260db73028c2b858dd3fdc51a0d
SADHANA M <sana27122006@gmail.com>
2026-04-07 21:39:48 +0530

    Add case study 5

---
+++ src/casestudy5.ts
@@ -0,0 +1,14 @@
+function recordAnswer(questionId :number, answer:any){
+    return {
+        questionId, //object
+        answer // object
+    };
+
+}
+let answer1= recordAnswer(1, "Hello"); // using string
+let answer2= recordAnswer(2, 42); // numbers
+let answer3= recordAnswer (3,["yes", "no"]); //array
+console.log(answer1);
+console.log(answer2);
+console.log(answer3);
+

8a4ea0d8cc662cd288563d6674d2ec16adec78e6
SADHANA M <sana27122006@gmail.com>
2026-04-08 22:02:41 +0530

    casestudy 6 in typescript

---
+++ src/casestudy6.ts
@@ -0,0 +1,19 @@
+function processTransaction(
+  amount: number,
+  isCredit: boolean,
+  description?: string
+): void {
+if(amount<0) {
+    throw new Error("Amount cannot be negative");
+}
+const desc: string = description ?? "No description provided";
+const transType: string = isCredit ? "Credit" : "Debit";
+console.log("Transaction Summary");
+console.log(`Amount: ${amount}`);
+console.log(`Description: ${desc}`);
+console.log(`Type: ${transType}`);
+}
+processTransaction(100, true, "salary");
+processTransaction(300, true); // description optional
+processTransaction(-200, false); // negative number
+

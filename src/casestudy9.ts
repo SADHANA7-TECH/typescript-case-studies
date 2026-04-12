6979a052d668d18d9a860d7a9acb08945d83b4e2
SADHANA M <sana27122006@gmail.com>
2026-04-12 22:20:42 +0530

    case study 9 in ts

---
+++ src/casestudy9.ts
@@ -0,0 +1,28 @@
+type CustomerID= string;
+
+
+type Customer ={
+    id: CustomerID;
+    name: string;
+    email?: string;
+};
+
+type OrderStatus ="pending" | "shipped" | "returned";
+
+type ProcessOrder = (orderId: number, callback: (status: OrderStatus) => void) => void;
+type Container<T> = {
+    value: T;
+    timestamp: Date;
+};
+let customer: Customer = {
+  id: "C101",
+  name: "Sana",
+ 
+};
+
+let customerContainer: Container<Customer> = {
+  value: customer,
+  timestamp: new Date()
+};
+console.log(customer);
+console.log(customerContainer);

1665c4ddb1d1a1a66401cfcc3ee700ed14aec292
SADHANA M <sana27122006@gmail.com>
2026-04-14 23:00:02 +0530

    case study 16 in ts

---
+++ src/casestudy16.ts
@@ -0,0 +1,46 @@
+interface Observer {
+  update(msg: string): void;
+}
+
+class Customer implements Observer {
+  update(msg: string) {
+    console.log("Customer:", msg);
+  }
+}
+
+class Inventory implements Observer {
+  update(msg: string) {
+    console.log("Inventory:", msg);
+  }
+}
+
+
+class PromotionSystem implements Observer {
+  update(msg: string) {
+    console.log("Promotion:", "Special offer triggered because ->", msg);
+    console.log("Promotion:", "Get 10% off on your next drink!");
+  }
+}
+
+class DrinkOrder {
+  private observers: Observer[] = [];
+
+  addObserver(obs: Observer) {
+    this.observers.push(obs);
+  }
+
+  notifyAll(msg: string) {
+    this.observers.forEach(obs => obs.update(msg));
+  }
+
+  completeOrder() {
+    this.notifyAll("Order complete!");
+  }
+}
+const order = new DrinkOrder();
+
+order.addObserver(new Customer());
+order.addObserver(new Inventory());
+order.addObserver(new PromotionSystem()); 
+
+order.completeOrder();

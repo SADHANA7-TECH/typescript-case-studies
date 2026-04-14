ce11f10f4a32e7466a60033a96c6dff6710f2a48
SADHANA M <sana27122006@gmail.com>
2026-04-14 22:02:43 +0530

    casestudy 13 in ts

---
+++ src/casestudy13.ts
@@ -0,0 +1,21 @@
+function describePerson(name: string, age?: number): void {
+    if (age !== undefined) {
+        console.log(`Name: ${name}, Age: ${age}`);
+    } else {
+        console.log(`Name: ${name}, Age: Unknown`);
+    }
+}
+//describePerson("Sana", 25);
+//describePerson("Sam");
+
+
+function calculatePrice(basePrice:number, discount: number=0.1): number{
+    return basePrice-(basePrice*discount);
+}
+
+
+describePerson("Eve");
+describePerson("Frank", 28);
+
+console.log("Price after discount:", calculatePrice(100));
+console.log("price with 20% discount:", calculatePrice(100, 0.2));

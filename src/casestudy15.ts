30ab0759a93b5d7774934d195f12492f5078fd33
SADHANA M <sana27122006@gmail.com>
2026-04-14 22:46:55 +0530

    case study 15 in ts

---
+++ src/casestudy15.ts
@@ -0,0 +1,33 @@
+type Instructor = { id: string; coursesTaught: number };
+type Admin = { id: string; accessLevel: "basic" | "super" };
+
+type InstructorOrAdmin = Instructor | Admin;
+
+
+type Assignment = {
+  title: string;
+  dueDate: Date;
+  points: number;
+};
+
+type ReadonlyAssignment = Readonly<Assignment>;
+const assignment1: ReadonlyAssignment = {
+  title: "Math",
+  dueDate: new Date(),
+  points: 100
+};
+
+const assignment2: ReadonlyAssignment = {
+  title: "Science",
+  dueDate: new Date(),
+  points: 23
+};
+
+type LearnerStats = {
+  quizzes: number;
+  videos: number;
+  assignments: number;
+};
+type StatsAsStrings = {
+  [K in keyof LearnerStats]: string;
+};

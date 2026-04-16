ea53039ffc22f774780ae36fadff97ea106dd495
SADHANA M <sana27122006@gmail.com>
2026-04-16 22:24:02 +0530

    case study17 in ts

---
+++ src/casestudy19/tokens.ts
@@ -0,0 +1,3 @@
+import { NewsSource } from "./NewsSource";
+
+export const NEWS_SOURCE = "NEWS_SOURCE";
--- src/tokens.ts
+++
@@ -1,4 +0,0 @@
-import { Token } from "typedi";
-import { NewsSource } from "./NewsSource";
-
-export const NEWS_SOURCE = new Token<NewsSource>("NEWS_SOURCE");

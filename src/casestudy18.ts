b5c91951b51cae8a97f1d48a8f731c68f6966b45
SADHANA M <sana27122006@gmail.com>
2026-04-16 21:17:25 +0530

    casestudy18 in ts

---
+++ src/casestudy18.ts
@@ -0,0 +1,40 @@
+interface PaymentGateway {
+  processPayment(amount: number): Promise<boolean>;
+}
+
+class PaymentProcessor {
+  constructor(private gateway: PaymentGateway) {}
+
+  async pay(amount: number): Promise<void> {
+    const success = await this.gateway.processPayment(amount);
+    if (success) {
+      console.log("Payment successful!");
+    } else {
+      console.log("Payment failed.");
+    }
+  }
+}     //BankTransferGateway
+class BankTransferGateway implements PaymentGateway {
+  async processPayment(amount: number): Promise<boolean> {
+    console.log(`Processing payment of $${amount} via Bank Transfer.`);
+    return true;
+  }
+}
+
+// Using  PaymentProcessor
+const bankGateway = new BankTransferGateway();
+const processor = new PaymentProcessor(bankGateway);
+processor.pay(300);
+
+// testing the mock failure gateway
+class FailingMockGateway implements PaymentGateway {
+  async processPayment(amount: number): Promise<boolean> {
+    console.log(`Mock processing payment of $${amount} - FAILED.`);
+    return false;
+  }
+}
+
+    // Test error handling
+const failingGateway = new FailingMockGateway();
+const testProcessor = new PaymentProcessor(failingGateway);
+testProcessor.pay(50);
